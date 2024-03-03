bf606f314b4c81ffa9ba0963ea73545fcc1d3951
Jerry <superjerry88@hotmail.com>
2024-03-04 03:42:36 +0800

    Added base project with blazorised, tailwind and fa

---
+++ WebApp/Items.cs
@@ -0,0 +1,31 @@
+﻿namespace HackathonReceiptScanner
+{
+    public class Receipt
+    {
+        public List<Items> Items { get; set; }
+        public string ShopName { get; set; }
+        public string ShopRegisteredNumber { get; set; }
+        public string Address { get; set; }
+        public DateTime DateTime { get; set; }
+
+        public string OperatorName { get; set; }
+        public int ReturnPeriod { get; set; }//IF valid
+
+        public double IsTaxShown { get; set; }
+        public double TaxPercentage { get; set; }
+        public double TotalTax { get; set; }
+        public double TotalPrice { get; set; }
+        public double TotalPriceWithoutTax { get; set; }
+        public Dictionary<string,string> OtherValues { get; set; }
+    }
+    public class Items
+    {
+        public string? Name { get; set; }
+        public string? Category { get; set; } //Food, Clothing, Service ...
+        public string? Description { get; set; } //GPT created description
+        public double Price { get; set; }
+        public int Quantity { get; set; }
+        public double TotalPrice => Price * Quantity;
+        public bool TaxInclusive {get; set; }
+    }
+}
---
+++ WebApp/Program.cs
@@ -0,0 +1,34 @@
+using Blazorise;
+using Blazorise.Icons.FontAwesome;
+using Blazorise.Tailwind;
+using WebApp.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+builder.Services
+    .AddBlazorise()
+    .AddTailwindProviders()
+    .AddFontAwesomeIcons();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
