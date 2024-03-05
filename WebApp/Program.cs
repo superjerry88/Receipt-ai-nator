9760eec50f80b2cbf36370f0d39436dc5f47635b
Jerry <superjerry88@hotmail.com>
2024-03-05 11:37:29 +0800

    Added config binding on startup

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -2,20 +2,25 @@ using Blazorise;
 using Blazorise.Icons.FontAwesome;
 using Blazorise.Tailwind;
 using WebApp.Components;
+using WebApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+// Add Blazorise
 builder.Services
     .AddBlazorise()
     .AddTailwindProviders()
     .AddFontAwesomeIcons();
 
-var app = builder.Build();
+// Add custom SiteSetting service
+builder.Configuration.AddSiteBinding();
 
 // Configure the HTTP request pipeline.
+var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
