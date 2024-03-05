1e48f09b6614be40fbc504c8c6200fbf8c5899ec
Jerry <superjerry88@hotmail.com>
2024-03-05 11:36:48 +0800

    Added service to load SiteSetting

---
+++ WebApp/Services/ConfigurationBuilderExtensions.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApp.Services;
+
+public static class ConfigurationBuilderExtensions
+{
+    public static IConfigurationBuilder AddSiteBinding(this IConfigurationBuilder builder)
+    {
+
+        var configuration = builder.Build();
+        configuration.GetSection("SiteSetting").Bind(RezApi.Settings);
+        RezApi.Setup();
+        return builder;
+    }
+}
---
+++ WebApp/Services/RezApi.cs
@@ -0,0 +1,12 @@
+﻿namespace WebApp.Services
+{
+    public static class RezApi
+    {
+        public static SiteSetting Settings { get; set; } = new SiteSetting();
+
+        internal static void Setup()
+        {
+            //todo 
+        }
+    }
+}
