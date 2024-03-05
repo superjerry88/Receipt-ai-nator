fc03446095043bc62f495bd476197c8744564cf8
Jerry <superjerry88@hotmail.com>
2024-03-05 11:36:25 +0800

    Added SiteSetting class

---
+++ WebApp/SiteSetting.cs
@@ -0,0 +1,32 @@
+﻿namespace WebApp
+{
+    public class SiteSetting
+    {
+        public string DbConnectionString { get; set; } = "";
+
+        public string DbName { get; set; } = "";
+
+        public string StoragePath { get; set; } = "";
+
+
+        public string OpenAiApiKey { get; set; } = "";
+
+        /// <summary>
+        /// Resetting the JWT Seed will invalidate all the existing tokens
+        /// </summary>
+        public string JwtSeed { get; set; } = "";
+
+        //Temporary
+
+        /// <summary>
+        /// For Python Image Processing Endpoint
+        /// </summary>
+        public string ImageProcessingEndpoint { get; set; } = "";
+
+        /// <summary>
+        /// For Python OCR Processing Endpoint
+        /// </summary>
+        public string OcrProcessingEndpoint { get; set; } = "";
+
+    }
+}
