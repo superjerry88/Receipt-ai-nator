e4c4e58ab5cc48e0ad10922ee7237e8307e4009f
Jerry <superjerry88@hotmail.com>
2024-03-09 11:32:49 +0800

    Added simple user and DbUser class

---
+++ WebApp/DB/DbUser.cs
@@ -0,0 +1,36 @@
+﻿using MongoDB.Driver;
+using WebApp.DB.Core;
+using WebApp.Model;
+using MongoDB.Driver.Linq;
+
+namespace WebApp.DB;
+
+public class DbUser : DbBase
+{
+    public DbUser(MongoConnector connector) : base(connector)
+    {
+
+    }
+
+    private IMongoCollection<User> Collection => GetCollection<User>();
+
+    public async Task AddUser(User user)
+    {
+        await Collection.InsertOneAsync(user);
+    }
+
+    public async Task<List<User>> GetAllUser()
+    {
+        return await Collection.AsQueryable().ToListAsync();
+    }
+
+    public async Task<User> GetUser(string userId)
+    {
+        return await Collection.AsQueryable().FirstOrDefaultAsync(x => x.Id == userId);
+    }
+
+    public async Task<User> GetByUsername(string username)
+    {
+        return await Collection.AsQueryable().FirstOrDefaultAsync(x => x.Username == username);
+    }
+}
---
+++ WebApp/Model/User.cs
@@ -0,0 +1,15 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+
+namespace WebApp.Model
+{
+    /// <summary>
+    /// Simple user object
+    /// </summary>
+    public class User
+    {
+        [BsonId]
+        public string Id { get; set; } = Guid.NewGuid().ToString("N");
+        public string Username { get; set; }
+        public string HashedPassword { get; set; }
+    }
+}
