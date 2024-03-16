5dd13c9cc43f7f8908b0a9e9f21427b38e8fb88b
Jerry <superjerry88@hotmail.com>
2024-03-16 20:46:16 +0800

    Added FileService for handling local storage

---
+++ WebApp/Services/FileService.cs
@@ -0,0 +1,24 @@
+﻿namespace WebApp.Services;
+
+public class FileService
+{
+    private const string UnsafeFolderName = "unsafe";
+    private const string ResponseFolderName = "response";
+    public string UnsafeFolderPath => Path.Combine(RezApi.Settings.StoragePath, UnsafeFolderName);
+    public string ResponseFolderPath => Path.Combine(RezApi.Settings.StoragePath, ResponseFolderName);
+
+    public string UnsafeFolderUrl(string fileName) => Path.Combine("files", UnsafeFolderName, fileName); 
+    public string ResponseFolderUrl(string fileName) => Path.Combine("files", ResponseFolderName, fileName);
+
+
+    public void Initialize()
+    {
+        if (!Directory.Exists(RezApi.Settings.StoragePath))
+        {
+            Directory.CreateDirectory(RezApi.Settings.StoragePath);
+        }
+        Directory.CreateDirectory(UnsafeFolderPath);
+        Directory.CreateDirectory(ResponseFolderPath);
+    }
+        
+}
