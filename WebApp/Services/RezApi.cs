fc62ff41cb4164e03379171da5bb3300f8714924
Jerry <superjerry88@hotmail.com>
2024-03-16 20:46:31 +0800

    Added file service to RezApi

--- WebApp/Services/RezApi.cs
+++ WebApp/Services/RezApi.cs
@@ -5,11 +5,13 @@ namespace WebApp.Services
     public static class RezApi
     {
         public static SiteSetting Settings { get; set; } = new SiteSetting();
+        public static FileService Files { get; set; } = new FileService();
         public static DbManager DbManager { get; set; } = new DbManager();
 
         internal static void Setup()
         {
              DbManager.InitializeDb();
+             Files.Initialize();
         }
     }
 }
