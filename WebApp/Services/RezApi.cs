a85b81a9b5c377e3a661de9a317099a86bd585c9
Jerry <superjerry88@hotmail.com>
2024-03-17 21:40:16 +0800

    Updated RezApi to include JwtHelper

--- WebApp/Services/RezApi.cs
+++ WebApp/Services/RezApi.cs
@@ -7,6 +7,7 @@ namespace WebApp.Services
         public static SiteSetting Settings { get; set; } = new SiteSetting();
         public static FileService Files { get; set; } = new FileService();
         public static DbManager DbManager { get; set; } = new DbManager();
+        public static JwtHelper JwtHelper { get; set; } = new JwtHelper();
 
         internal static void Setup()
         {
