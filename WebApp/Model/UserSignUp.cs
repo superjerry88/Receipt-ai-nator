7d0a98a7702f3e5df42192b897ce28698dc4b6a9
Jerry <superjerry88@hotmail.com>
2024-03-17 21:36:14 +0800

    Added UserSignUp class

---
+++ WebApp/Model/UserSignUp.cs
@@ -0,0 +1,39 @@
+﻿using WebApp.DB;
+using WebApp.Services;
+
+namespace WebApp.Model;
+
+public class UserSignUp
+{
+    public string Email { get; set; } = "";
+    public string Username { get; set; } = "";
+    public string Password { get; set; } = "";
+    public string PasswordConfirm { get; set; } = "";
+
+    public bool MatchingPassword()
+    {
+        if (string.IsNullOrEmpty(Password)) return false;
+        return Password == PasswordConfirm;
+    }
+
+    public async Task<bool> ValidateUsername()
+    {
+        if(string.IsNullOrEmpty(Username)) return false;
+        var user = await RezApi.DbManager.Get<DbUser>().GetByUsername(Username);
+        return user == null;
+    }
+
+    public async Task<bool> ValidateEmail()
+    {
+        if(string.IsNullOrEmpty(Email)) return false;
+        var user = await RezApi.DbManager.Get<DbUser>().GetByEmail(Email);
+        return user == null;
+    }
+
+    public async Task<User> Signup()
+    {
+        var user = new User(this);
+        await RezApi.DbManager.Get<DbUser>().AddUser(user);
+        return user;
+    }
+}
