36ad0d9c896531294196ff12da35fea4ceed4e00
Jerry <superjerry88@hotmail.com>
2024-03-17 21:39:37 +0800

    Added Cookie Service

---
+++ WebApp/Services/CookieService.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.JSInterop;
+using WebApp.DB.Core;
+
+namespace WebApp.Services;
+
+public class CookieService(IJSRuntime jsRuntime)
+{
+    private const string CookieName = "Session";
+
+    public async Task<string> GetSession()
+    {
+        return await jsRuntime.InvokeAsync<string>("localStorage.getItem", CookieName);
+    }
+
+    public async Task SetCookie(string currency)
+    {
+        await jsRuntime.InvokeAsync<object>("localStorage.setItem", CookieName, currency);
+    }
+
+    public async Task DeleteCookie()
+    {
+        await jsRuntime.InvokeAsync<object>("localStorage.removeItem", CookieName);
+    }
+}
