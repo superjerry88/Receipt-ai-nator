c1610a9e5e3d05796d9a255e53ab7f1e23298f8e
Jerry <superjerry88@hotmail.com>
2024-03-17 21:42:15 +0800

    Added DbGptUsage for tracking API consumptions

---
+++ WebApp/DB/DbGptUsage.cs
@@ -0,0 +1,20 @@
+﻿using MongoDB.Driver;
+using OpenAI.ObjectModels.ResponseModels;
+using WebApp.DB.Core;
+
+namespace WebApp.DB;
+
+public class DbGptUsage(MongoConnector connector) : DbBase(connector)
+{
+    private IMongoCollection<ChatCompletionCreateResponse> Collection => GetCollection<ChatCompletionCreateResponse>();
+
+    public async Task AddUsage(ChatCompletionCreateResponse usage)
+    {
+        await Collection.InsertOneAsync(usage);
+    }
+
+    public async Task<List<ChatCompletionCreateResponse>> GetAllUsage()
+    {
+        return await Collection.AsQueryable().ToListAsync();
+    }
+}
