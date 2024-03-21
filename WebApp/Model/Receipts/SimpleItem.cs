a1e80863fe368b9dd44b9b13b625c5e44758073c
Jerry <superjerry88@hotmail.com>
2024-03-21 16:29:27 +0800

    Added SimpleReceipt class to save tokens

---
+++ WebApp/Model/Receipts/SimpleItem.cs
@@ -0,0 +1,14 @@
+﻿namespace WebApp.Model.Receipts;
+
+public class SimpleItem
+{
+    public SimpleItem(Items item)
+    {
+        Name = item.Name!;
+        Quantity = item.Quantity;
+        TotalPrice = item.TotalPrice;
+    }
+    public string Name { get; set; }
+    public int Quantity { get; set; }
+    public double TotalPrice  { get; set; }
+}
---
+++ WebApp/Model/Receipts/SimpleReceipt.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApp.Model.Receipts;
+
+public class SimpleReceipt
+{
+    public SimpleReceipt(Receipt receipt)
+    {
+        Items = new List<SimpleItem>(receipt.Items.Select(c => new SimpleItem(c)));
+        ShopName = receipt.ShopName;
+        ShopAddress = receipt.ShopAddress;
+        DateTime = receipt.DateTime;
+        TotalPrice = (int)receipt.TotalPrice;
+        Category = receipt.Category;
+        OtherValues = receipt.OtherValues;
+    }
+    public List<SimpleItem> Items { get; set; }
+    public string ShopName { get; set; }
+    public string ShopAddress { get; set; }
+    public DateTime DateTime { get; set; }
+    public double TotalPrice { get; set; }
+    public string Category { get; set; }
+    public Dictionary<string, string> OtherValues { get; set; }
+}
