26a2bb0aa9c63f2fd36094c780a7cc082632ae1f
daniNya <siewdaniel@yahoo.com>
2024-03-21 18:21:36 +0800

    Updated StringExtensions to allow for easy currency formatting

---
+++ WebApp/Services/StringExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApp.Services
+{
+    public static class StringExtensions
+    {
+        public static string FixString(this object? input, bool isCurrency = false)
+        {
+            if (input == null)
+            {
+                return "";
+            }
+
+            var word = input.ToString();
+
+            if (isCurrency)
+            {
+                word = $"RM{String.Format("{0:0.00}", Convert.ToDecimal((double)input * 100) / 100)}";
+            }
+
+            return string.IsNullOrEmpty(word) ? "Not Available" : word;
+        }
+    }
+}
